b9479813ed8f41e423137975db1e869002fb2d72
Yuvraj Das <48616872+yuvrajdas@users.noreply.github.com>
2023-01-14 19:18:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const microphoneButton = document.getElementById('microphoneButton');
 
 microphoneButton.addEventListener('click',function(){
     window.open(
-        'https://dreamy-sunshine-379868.netlify.app/mic/index1.html',
+        'https://jarvis-by-yuvraj.netlify.app/mic/index1.html',
         "",
         "width=280px,height=280px"
     );
