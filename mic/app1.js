b9a5b4198dc3c63c01b50998c3d11e979217498d
Yuvraj Das <48616872+yuvrajdas@users.noreply.github.com>
2023-01-14 10:19:52 +0530

    Update app1.js

--- mic/app1.js
+++ mic/app1.js
@@ -1,8 +1,14 @@
 let Commands;
 const API_KEY="8dfd963431ab9f8e4b930973408b8e15";
 const one = "Chaand Taare - Yes Boss 128 Kbps.mp3";
-const two = "Chand_Tare_Tod_Full_Video_Song.mp3";
 const three = "First-Love--Pyar--Sunny-Cheema.mp3";
+const four = "Let Me Love You_64(PagalWorld.com.se).mp3";
+const five = "Meri jindagi h tu lyrics song.best status song.For 1 min.sad song.mp4";
+const six = "Satisfya English Version_64(PagalWorld.com.se).mp3";
+const seven = "YALGAAR - CARRYMINATI X Wily Frenzy.mp4";
+const eight = "Senorita_64(PagalWorld.com.se).mp3";
+const eleven = "";
+
  
 function fetchCommands()
 {
